f803ea932618b391517531e508b5ea4f8fda1d2c
Famil Babayev <familhacker@gmail.com>
2021-09-13 20:21:27 +0400

    converted to async/await

--- source/app.js
+++ source/app.js
@@ -34,14 +34,16 @@ app.get('/', async (req, res) => {
         const customers_num = await query("SELECT COUNT(*) as count FROM customers");
         const products_num = await query("SELECT COUNT(*) as count FROM products");
         const orders_num = await query("SELECT COUNT(*) count FROM orders");
-        const some_query = await query(`select orders.product_id, products.price, orders.quantity
-        from orders, products
-        where orders.product_id = products.product_id`);
+        const total_sales = await query(`SELECT orders.product_id, products.price, orders.quantity
+                                        FROM orders, products
+                                        WHERE orders.product_id = products.product_id`);
         let total = 0;
-        some_query.forEach((item) => { total += item.price * item.quantity })
-        res.render('index', {customers_num:customers_num, products_num:products_num, orders_num:orders_num, total:total.toFixed(2)});
-    } catch (err) {
-        throw err;
+        total_sales.forEach((item) => { total += item.price * item.quantity })
+
+        res.render('index', {customers_num:customers_num, products_num:products_num, 
+                            orders_num:orders_num, total:total.toFixed(2)});
+    } catch (error) {
+        throw error;
     }
 });
 
@@ -52,7 +54,7 @@ app.get('/customers', async (req, res) => {
 
         res.render('customers', {customers:customers});
     } catch (error) {
-        throw err;
+        throw error;
     }
 });
 
@@ -70,37 +72,46 @@ app.post('/customer/add', async (req, res) => {
     var street = req.body.street;
     var zipcode = req.body.zipcode;
 
-    var sql = `INSERT INTO customers (name, email, phone, city, street, zipcode) 
+    let sql = `INSERT INTO customers (name, email, phone, city, street, zipcode) 
     VALUES('${name}', '${email}', '${phone}', '${city}', '${street}', '${zipcode}')`;
+
     try{
         await query(sql);
-    } catch (err) {
-        throw err;
+
+        res.redirect('/customers');
+    } catch (error) {
+        throw error;
     }
-
-    res.redirect('/customers');
 });
 
 
-app.post('/customer/delete/:customer_id', (req, res) => {
-    conn.query('DELETE FROM `customers` WHERE customer_id = ?', [req.params.customer_id], (err, results) => {
-        if (err) throw err;
-        else{
-            res.redirect('/customers');
-        }
-    });
+app.post('/customer/delete/:customer_id', async (req, res) => {
+    let sql = `DELETE FROM customers WHERE customer_id = ${req.params.customer_id}`;
+
+    try{
+        await query(sql);
+
+        res.redirect('/customers');
+    } catch (error) {
+        throw error;
+    }
 });
 
 
-app.get('/customer/edit/:customer_id', (req, res) => {
-    conn.query('SELECT * FROM `customers` WHERE customer_id=?',[req.params.customer_id] , (err, result) => {
-        if (err)  throw err;
-        res.render('edit_customer',{customer:result[0]});
-    });
+app.get('/customer/edit/:customer_id', async (req, res) => {
+    let sql = `SELECT * FROM customers WHERE customer_id = ${req.params.customer_id}`;
+
+    try{
+        const customer = await query(sql);
+
+        res.render('edit_customer',{customer:customer[0]});
+    } catch (error) {
+        throw error;
+    }
 });
 
 
-app.post('/customer/edit/:customer_id', (req, res) => {
+app.post('/customer/edit/:customer_id', async (req, res) => {
     const update_name = req.body.name;
     const update_email = req.body.email;
     const update_phone = req.body.phone;
@@ -109,25 +120,30 @@ app.post('/customer/edit/:customer_id', (req, res) => {
     const update_zipcode = req.body.zipcode;
 
     const customer_id = req.params.customer_id;
-    conn.query('UPDATE `customers` SET name=?, email=?, phone=?, city=?, street=?, zipcode=? WHERE customer_id=?', 
-    [update_name, update_email, update_phone, update_city, update_street, update_zipcode, customer_id], (err, results) => {
-        if (err) throw err;
-        if(results.changedRows === 1){
-            return res.redirect('/customers');
-        }
-    });
+
+    let sql = `UPDATE customers SET name='${update_name}', email='${update_email}', phone='${update_phone}', 
+    city='${update_city}', street='${update_street}', zipcode='${update_zipcode}' WHERE customer_id='${customer_id}'`;
+
+    try{
+        await query(sql);
+
+        return res.redirect('/customers');
+    } catch (error) {
+        throw error;
+    }
 });
 
 
 // Product routes
 
-app.get('/products', (req, res) => {
-    conn.query("SELECT * FROM products", function(err, result){
-        if (err) throw err;
-        else{
-            res.render('products', {products:result});
-        }
-    })
+app.get('/products', async (req, res) => {
+    try{
+        const products = await query("SELECT * FROM products");
+
+        res.render('products', {products:products});
+    } catch (error) {
+        throw error;
+    }
 });
 
 app.get('/product/add', (req, res) => {
@@ -135,136 +151,155 @@ app.get('/product/add', (req, res) => {
 })
 
 
-app.post('/product/add', (req, res) => {
+app.post('/product/add', async (req, res) => {
     var name = req.body.name;
     var quantity = req.body.quantity;
     var price = req.body.price;
 
-    var sql = "INSERT INTO products (name, quantity, price) VALUES('"+name+"', '"+quantity+"', '"+price+"')";
-    conn.query(sql, function(err, result){
-        if (err) throw err;
-        res.end();
-    });
-    res.redirect('/products');
+    let sql = `INSERT INTO products (name, quantity, price) 
+    VALUES('${name}', '${quantity}', '${price}')`;
+
+    try{
+        await query(sql);
+
+        res.redirect('/products');
+    } catch (error) {
+        throw error;
+    }
 });
 
 
-app.post('/product/delete/:product_id', (req, res) => {
-    conn.query('DELETE FROM `products` WHERE product_id = ?', [req.params.product_id], (err, results) => {
-        if (err) throw err;
-        else{
-            res.redirect('/products');
-        }
-    });
+app.post('/product/delete/:product_id', async (req, res) => {
+    let sql = `DELETE FROM products WHERE product_id = ${req.params.product_id}`;
+
+    try{
+        await query(sql);
+
+        res.redirect('/products');
+    } catch (error) {
+        throw error;
+    }
 });
 
 
-app.get('/product/edit/:product_id', (req, res) => {
-    conn.query('SELECT * FROM `products` WHERE product_id=?',[req.params.product_id] , (err, result) => {
-        if (err)  throw err;
-        res.render('edit_product',{product:result[0]});
-    });
+app.get('/product/edit/:product_id', async (req, res) => {
+    let sql = `SELECT * FROM products WHERE product_id = ${req.params.product_id}`;
+
+    try{
+        const product = await query(sql);
+
+        res.render('edit_product',{product:product[0]});
+    } catch (error) {
+        throw error;
+    }
 });
 
 
-app.post('/product/edit/:product_id', (req, res) => {
+app.post('/product/edit/:product_id', async (req, res) => {
     const update_name = req.body.name;
     const update_quantity = req.body.quantity;
     const update_price = req.body.price;
 
     const product_id = req.params.product_id;
-    conn.query('UPDATE `products` SET name=?, quantity=?, price=? WHERE product_id=?', 
-    [update_name, update_quantity, update_price, product_id], (err, results) => {
-        if (err) throw err;
-        if(results.changedRows === 1){
-            console.log('Post Updated');
-            return res.redirect('/products');
-        }
-    });
+
+    let sql = `UPDATE products SET name='${update_name}', quantity='${update_quantity}', 
+                price='${update_price}' WHERE product_id='${product_id}'`;
+
+    try{
+        await query(sql);
+
+        return res.redirect('/products');
+    } catch (error) {
+        throw error;
+    }
 });
 
 
 // Order routes
-app.get('/orders', (req, res) => {
-    conn.query("SELECT * FROM orders", function(err, results){
-        if (err) throw err;
-        else{
-            res.render('orders', {orders:results});
-        }
-    });
+app.get('/orders', async (req, res) => {
+    try{
+        const orders = await query("SELECT * FROM orders");
+
+        res.render('orders', {orders:orders});
+    } catch (error) {
+        throw error;
+    }
 });
 
 
-app.get('/order/add', (req, res) => {
-    conn.query("SELECT customer_id FROM customers", function(err, results1){
-        if (err) throw err;
-        else{
-            conn.query("SELECT product_id FROM products", function(err, results2){
-                if (err) throw err;
-                else{
-                    res.render('add_order', {customer_ids:results1, product_ids:results2});
-                }
-            })
-        }
-    });
+app.get('/order/add', async (req, res) => {
+    try{
+        let customer_ids = await query("SELECT customer_id FROM customers");
+        let product_ids = await query("SELECT product_id FROM products");
+
+        res.render('add_order', {customer_ids:customer_ids, product_ids:product_ids})
+    } catch(error) {
+        throw error;
+    }
 });
 
 
-app.post('/order/add', (req, res) => {
+app.post('/order/add', async (req, res) => {
     var selected_cus = req.body.customer_ids;
     var selected_pro = req.body.product_ids;
     var quantity = req.body.quantity;
 
-    var sql = "INSERT INTO orders (customer_id, product_id, quantity) VALUES('"+selected_cus+"', '"+selected_pro+"', '"+quantity+"')";
-    conn.query(sql, function(err, result){
-        if (err) throw err;
-        res.end();
-    });
-    res.redirect('/orders');
+    let sql = `INSERT INTO orders (customer_id, product_id, quantity) 
+    VALUES('${selected_cus}', '${selected_pro}', '${quantity}')`;
+
+    try{
+        await query(sql);
+
+        res.redirect('/orders');
+    } catch (error) {
+        throw error;
+    }
 });
  
 
-app.post('/order/delete/:order_id', (req, res) => {
-    conn.query('DELETE FROM `orders` WHERE order_id = ?', [req.params.order_id], (err, results) => {
-        if (err) throw err;
-        else{
-            res.redirect('/orders');
-        }
-    });
+app.post('/order/delete/:order_id', async (req, res) => {
+    let sql = `DELETE FROM orders WHERE order_id = ${req.params.order_id}`;
+
+    try{
+        await query(sql);
+
+        res.redirect('/orders');
+    } catch (error) {
+        throw error;
+    }
 });
 
 
-app.get('/order/edit/:order_id', (req, res) => {
-    conn.query("SELECT customer_id FROM customers", function(err, results1){
-        if (err) throw err;
-        else{
-            conn.query("SELECT product_id FROM products", function(err, results2){
-                if (err) throw err;
-                else{
-                    conn.query('SELECT * FROM `orders` WHERE order_id=?',[req.params.order_id] , (err, result) => {
-                        if (err)  throw err;
-                        res.render('add_order', {customer_ids:results1, product_ids:results2, order:result[0]});
-                    });
-                }
-            })
-        }
-    });
+app.get('/order/edit/:order_id', async (req, res) => {
+    try{
+        let customer_ids = await query("SELECT customer_id FROM customers");
+        let product_ids = await query("SELECT product_id FROM products");
+        let order = await query(`SELECT * FROM orders WHERE order_id='${req.params.order_id}'`)
+
+        res.render('edit_order', {customer_ids:customer_ids, product_ids:product_ids, order:order[0]})
+    } catch(error) {
+        throw error;
+    }
 });
 
 
-app.post('/order/edit/:order_id', (req, res) => {
+app.post('/order/edit/:order_id', async (req, res) => {
     const update_cus_id = req.body.customer_ids;
     const update_pro_id = req.body.product_ids;
     const update_quantity = req.body.quantity;
 
     const order_id = req.params.order_id;
-    conn.query('UPDATE `orders` SET customer_id=?, product_id=?, quantity=? WHERE order_id=?', 
-    [update_cus_id, update_pro_id, update_quantity, order_id], (err, results) => {
-        if (err) throw err;
-        if(results.changedRows === 1){
-            return res.redirect('/orders');
-        }
-    });
+
+    let sql = `UPDATE orders SET customer_id='${update_cus_id}', product_id='${update_pro_id}', 
+                quantity='${update_quantity}' WHERE order_id='${order_id}'`;
+
+    try{
+        await query(sql);
+
+        return res.redirect('/orders');
+    } catch (error) {
+        throw error;
+    }
 });
 
 
